a138f45ee41d8c9432fe4a10ad7eb3c972e8d430
casariz <danielfelipedfco@gmail.com>
2024-12-16 19:54:00 -0500

    Add feature create proveedores and change model

--- src/models/ProveedoresModels.py
+++ src/models/ProveedoresModels.py
@@ -1,23 +1,31 @@
 from pydantic import BaseModel
 
-
 class Contacto(BaseModel):
     telefono: str
     email: str
 
 class ProductoSuministrados(BaseModel):
+    id: str
     nombre: str
     precioCompra: int
     precioUnitario: int
     categoria: str
 
+class TerminoEntrega(BaseModel):
+    plazo: str  # Ejemplo: "5 días hábiles"
+    metodo: str  # Ejemplo: "Transporte terrestre"
+
+class CondicionPago(BaseModel):
+    plazo: str  # Ejemplo: "30 días"
+    metodo: str  # Ejemplo: "Transferencia bancaria"
 
 class Proveedores(BaseModel):
-
     nombre: str
     direccion: str
     ciudad: str
     codigoPostal: str
     contacto: Contacto
     productosSuministrados: list[ProductoSuministrados]
+    terminoEntrega: TerminoEntrega
+    condicionPago: CondicionPago
     estado: int
